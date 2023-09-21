4b2538baa08032392cc7bb0fb58a7bbb03a2ef91
jungbk0808 <jungbk0808@gmail.com>
2023-09-21 15:03:38 +0900

    초기 커밋.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+#include "Segtree.h"
+#include <iostream>
+using namespace std;
+
+#define LIST_LEN 8
+
+int main() {
+	int list[LIST_LEN] = { 21, 7, 8, 19, 2, 9, 6, 10 };
+	int* seg_tree = Seg_tree_const(list, 8);
+	int q_s, q_e;
+	while (true) {
+		try {
+			cout << "------리스트의 부분 합 질의-----\n";
+			cout << "시작 인덱스 : "; cin >> q_s;
+			cout << "마지막 인덱스 : "; cin >> q_e;
+			cout << "질의 범위의 부분 합 : " << Get_query(seg_tree, LIST_LEN, q_s, q_e) << '\n';
+			break;
+		}
+		catch (int) { //객체로 변경????일단 다 짜고 생각하자.
+			cout << "리스트의 범위를 벗어난 질의입니다.\n";
+		}
+	}
+	int i, value;
+	while (true) {
+		try {
+			cout << "수정할 인덱스 : ";  cin >> i;
+			if (i < 0 || i > LIST_LEN) throw i;
+			break;
+		}
+		catch (int) {
+			cout << "수정할 인덱스가 리스트의 범위를 벗어났습니다.\n";
+		}
+	}
+	cout << "수정할 값 : "; cin >> value;
+	Segtree_update(seg_tree, 0, LIST_LEN - 1, i, value - list[i], 0);
+	
+	cout << "수정된 리스트의 질의 범위 부분 합 : " << Get_query(seg_tree, LIST_LEN, q_s, q_e);
+	return 0;
+}
