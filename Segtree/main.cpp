0ff10e7a712d0a2db50cbdffef0b061f1f55515c
jungbk0808 <jungbk0808@gmail.com>
2023-09-24 00:34:49 +0900

    modify main.cpp

--- Segtree/main.cpp
+++ Segtree/main.cpp
@@ -2,15 +2,15 @@
 #include <iostream>
 using namespace std;
 
-#define LIST_LEN 6
+#define LIST_LEN 8
 
 int main() {
-	int list[LIST_LEN] = { 2,4,10,7,9,20 };
+	int list[LIST_LEN] = { 21, 7, 8, 19, 2, 9, 6, 10 };
 	int* seg_tree = Seg_tree_const(list, LIST_LEN);
 	int q_s, q_e;
+	cout << "------리스트의 부분 합 질의-----\n";
 	while (true) {
 		try {
-			cout << "------리스트의 부분 합 질의-----\n";
 			cout << "시작 인덱스 : "; cin >> q_s;
 			cout << "마지막 인덱스 : "; cin >> q_e;
 			cout << "질의 범위의 부분 합 : " << Get_query(seg_tree, LIST_LEN, q_s, q_e) << '\n';
@@ -21,6 +21,7 @@ int main() {
 		}
 	}
 	int i, value;
+	cout << "----------리스트의 갱신---------\n";
 	while (true) {
 		try {
 			cout << "수정할 인덱스 : ";  cin >> i;
@@ -33,7 +34,6 @@ int main() {
 	}
 	cout << "수정할 값 : "; cin >> value;
 	Segtree_update(seg_tree, 0, LIST_LEN - 1, i, value - list[i], 0);
-	
 	cout << "수정된 리스트의 질의 범위 부분 합 : " << Get_query(seg_tree, LIST_LEN, q_s, q_e);
 	return 0;
 }
